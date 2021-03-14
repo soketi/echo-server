95743e2e7d1ccd58645163e5a3280ffd67b7c651
Alex Renoki <rennokki@gmail.com>
2021-03-14 17:22:14 +0200

    fixed id and path

--- tests/fixtures/api.js
+++ tests/fixtures/api.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/get-app', (req, res) => {
+app.get('/echo-server/app', (req, res) => {
     if (req.query.token !== process.env.APPS_MANAGER_TOKEN) {
         res.statusCode = 401;
         return res.json({ error: 'Unauthenticated' });
     }
 
-    if (req.query.appId && req.query.appId !== '1') {
+    if (!req.query.appId && !req.query.appKey) {
+        res.statusCode = 404;
+        return res.json({ app: null });
+    }
+
+    if (req.query.appId && req.query.appId !== 'echo-app') {
         res.statusCode = 404;
         return res.json({ app: null });
     }
@@ -20,7 +25,7 @@ app.get('/get-app', (req, res) => {
 
     res.json({
         app: {
-            id: 1,
+            id: 'echo-app',
             key: 'echo-app-key',
             secret: 'echo-app-secret',
             maxConnections: 100,
