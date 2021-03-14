8718b53bf8815754b02e95b89f1450c19d3f227e
Alex Renoki <rennokki@gmail.com>
2021-03-14 21:42:28 +0200

    Updated usage

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,8 +3,8 @@ import { Cli } from './cli';
 let cli = new Cli();
 
 let yargs = require('yargs')
-    .usage('Usage: laravel-echo-server <command> [options]')
-    .command('start', 'Starts the server.', yargs => cli.start(yargs))
+    .usage('Usage: echo-server <command> [options]')
+    .command('start', 'Start the server.', yargs => cli.start(yargs))
     .demandCommand(1, 'Please provide a valid command.')
     .help('help')
     .alias('help', 'h');
